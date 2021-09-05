dc0d288ce1bf69e907d271f60a68555f9115f5b5
aarsonmars <aarsonmars@gmail.com>
2021-09-05 20:53:58 +0545

    prime check

--- PrimeFactorization/primefactorization.js
+++ PrimeFactorization/primefactorization.js
@@ -2,5 +2,18 @@ function isPrime(num) {
     for(var i = 2; i < num; i++)
       if(num % i === 0) return false;
     return num > 1;
-    console.log('hello')
+    
   }
+
+function funt(e){
+    outputboxx=document.querySelector('h1')
+    outputboxx&&outputboxx.remove();
+    e.preventDefault();
+    num = document.querySelector('input').value;
+    out=isPrime(num)?'Prime':'Composite';
+    h1t=document.createElement('h1')
+    h1t.innerText=out;
+    document.body.append(h1t)
+}
+
+document.querySelector('button').addEventListener('click',(e)=>{funt(e)})
