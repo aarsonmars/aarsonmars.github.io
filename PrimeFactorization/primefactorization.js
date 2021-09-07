1fe61a411c57183909c1201a6a400da3923f93ad
aarsonmars <aarsonmars@gmail.com>
2021-09-07 16:07:01 +0545

    prime factorixation implementation

--- PrimeFactorization/primefactorization.js
+++ PrimeFactorization/primefactorization.js
@@ -1,19 +1,67 @@
-function isPrime(num) {
-    for(var i = 2; i < num; i++)
-      if(num % i === 0) return false;
-    return num > 1;
+function isPrime(n) {
+    i=2
+    while (i*i<n){
+        if (n%i==0){
+        return 0}
+      else{
+          i+=1   } }
+    return 1
     
   }
 
+
+function factorize(n){
+  factors=[]
+  divider=2
+  if (n<2){
+    return []
+  }
+  
+  while(n!=1){
+    if (n%divider==0){
+      factors.push(divider)
+      n=n/divider
+    }
+    else{
+      while (true){
+        if (isPrime(divider+1)==1){
+          divider+=1
+          break
+        }
+        else{
+          divider+=1
+        }
+      }
+    }
+  }
+  return factors
+}
+
+
+
 function funt(e){
-    outputboxx=document.querySelector('h1')
-    outputboxx&&outputboxx.remove();
     e.preventDefault();
+    ['h1','h3'].forEach((x)=>{outputboxx=document.querySelector(x);
+    outputboxx&&outputboxx.remove();})
     num = document.querySelector('input').value;
-    out=isPrime(num)?'Prime':'Composite';
-    h1t=document.createElement('h1')
-    h1t.innerText=out;
+    out=factorize(num)
+    h1t=document.createElement('h1');
+    h3t=document.createElement('h3');
+
+    if (out.length<1){
+      h1t.innerText = 'Not defined'
+    }
+    else if (out.length==1){
+      h1t.innerText=num + ' is '+  'Prime'
+    }
+    else{
+      h1t.innerText='Composite'
+      fac=out.join(' × ')
+      h3t.innerText= num + " = "+fac
+    }
+    
     document.body.append(h1t)
+    document.body.append(h3t)
 }
 
 document.querySelector('button').addEventListener('click',(e)=>{funt(e)})
