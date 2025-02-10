a4eb27bfe93dee2ba349057b45acf66f318e3b62
aarsonmars <aarsonmars@gmail.com>
2025-02-10 00:38:26 -0400

    update prime factorization interface and improve output display

--- PrimeFactorization/primefactorization.js
+++ PrimeFactorization/primefactorization.js
@@ -1,67 +1,63 @@
 function isPrime(n) {
-    i=2
-    while (i*i<n){
-        if (n%i==0){
-        return 0}
-      else{
-          i+=1   } }
+    i = 2
+    while (i * i < n) {
+        if (n % i == 0) {
+            return 0
+        } else {
+            i += 1
+        }
+    }
     return 1
-    
-  }
+}
 
 
-function factorize(n){
-  factors=[]
-  divider=2
-  if (n<2){
-    return []
-  }
-  
-  while(n!=1){
-    if (n%divider==0){
-      factors.push(divider)
-      n=n/divider
+function factorize(n) {
+    factors = []
+    divider = 2
+    if (n < 2) {
+        return []
     }
-    else{
-      while (true){
-        if (isPrime(divider+1)==1){
-          divider+=1
-          break
+
+    while (n != 1) {
+        if (n % divider == 0) {
+            factors.push(divider)
+            n = n / divider
+        } else {
+            while (true) {
+                if (isPrime(divider + 1) == 1) {
+                    divider += 1
+                    break
+                } else {
+                    divider += 1
+                }
+            }
         }
-        else{
-          divider+=1
-        }
-      }
     }
-  }
-  return factors
+    return factors
 }
 
 
 
-function funt(e){
+function funt(e) {
     e.preventDefault();
-    ['h1','h3'].forEach((x)=>{outputboxx=document.querySelector(x);
-    outputboxx&&outputboxx.remove();})
-    num = document.querySelector('input').value;
-    out=factorize(num)
-    h1t=document.createElement('h1');
-    h3t=document.createElement('h3');
+    const results = document.querySelector('.results');
+    results.innerHTML = '';
+    const num = document.querySelector('input').value;
+    const out = factorize(num);
+    const h1t = document.createElement('h1');
 
-    if (out.length<1){
-      h1t.innerText = 'Not defined'
+    if (out.length < 1) {
+        h1t.innerText = 'Not defined';
+    } else if (out.length === 1) {
+        h1t.innerHTML = 'Prime<br><span class="factors">&nbsp;</span>';
+    } else {
+        h1t.innerHTML = 'Composite<br><span class="factors">' 
+             + out.join(' × ') + '</span>';
     }
-    else if (out.length==1){
-      h1t.innerText=num + ' is '+  'Prime'
-    }
-    else{
-      h1t.innerText='Composite'
-      fac=out.join(' × ')
-      h3t.innerText= num + " = "+fac
-    }
-    
-    document.body.append(h1t)
-    document.body.append(h3t)
+    results.append(h1t);
 }
 
-document.querySelector('button').addEventListener('click',(e)=>{funt(e)})
+document.querySelector('button').addEventListener('click', (e) => { funt(e) })
+document.querySelector('input').addEventListener('input', (e) => {
+    funt(e);
+});
