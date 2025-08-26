14c9cc82f175b1aa83e7ea1234a441a932fcab86
aarsonmars <aarsonmars@gmail.com>
2025-08-26 20:25:27 -0300

    clean home page

---
+++ script.js
@@ -0,0 +1,16 @@
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+    burger.classList.toggle('toggle');
+});
+
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
